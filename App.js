ea1288453a0de37e923755b8f8dd534080eb4427
Naren Bakshi <narenbakshi@Narens-Air.lan>
2022-03-12 19:00:27 -0500

    search component added

--- App.js
+++ App.js
@@ -10,13 +10,18 @@ import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import Post from './src/components/Post';
 import HomeScreen from './src/screens/Home';
+import feed from './assets/data/feed';
+import SearchResult from './src/screens/SearchResults';
+import DestinationSearch from './src/screens/DestinationSearch';
+
+const post1 = feed[0];
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Post />
+        <DestinationSearch />
       </SafeAreaView>
     </>
   );
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,31 +3,31 @@ import {View, Text, Image} from 'react-native';
 import styles from './styles';
 
 const Post = props => {
+  const post = props.post;
+
   return (
     <View style={styles.container}>
       {/* Image */}
-      {/* <Image
-        style={styles.image}
-        source={require('../../../assets/images/wallpaper.jpg')}
-      /> */}
+      <Image style={styles.image} source={{uri: post.image}} />
 
       {/* Bed & Bedroom text */}
-      <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedrooms}>
+        {post.bed} bed {post.bedroom} bedroom
+      </Text>
 
       {/* Type and description */}
       <Text style={styles.description} numberOfLines={2}>
-        addsdasdasdasdasdnasdndasbd dasd dsad sdaad dasjdasd asdasjdas dasda
-        dawdada
+        {post.type}. {post.title}
       </Text>
 
       {/* old price & new price */}
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>$42</Text>
-        <Text style={styles.newPrice}> $30</Text> / night
+        <Text style={styles.oldPrice}>${post.oldPrice}</Text>
+        <Text style={styles.newPrice}> ${post.newPrice}</Text> / night
       </Text>
 
       {/* Total price */}
-      <Text style={styles.totalPrice}>$230 total</Text>
+      <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
     </View>
   );
 };
