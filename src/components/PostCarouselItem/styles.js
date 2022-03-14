f09dcbad4478355063cbe0e29e5acace9335f465
Naren Bakshi <narenbakshi@Narens-Air.lan>
2022-03-13 23:29:28 -0400

    Corousel menu added on maps

---
+++ src/components/PostCarouselItem/styles.js
@@ -0,0 +1,52 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    height: 120,
+    padding: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+
+    elevation: 9,
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  image: {
+    height: '100%',
+    aspectRatio: 1,
+    resizeMode: 'cover',
+  },
+  bedrooms: {
+    marginVertical: 10,
+    color: '#5b5b5b',
+  },
+  description: {
+    fontSize: 15,
+  },
+  prices: {
+    fontSize: 15,
+    marginVertical: 10,
+  },
+  oldPrice: {
+    color: '#5b5b5b',
+    textDecorationLine: 'line-through',
+  },
+  newPrice: {
+    fontWeight: 'bold',
+  },
+  totalPrice: {
+    color: '#5b5b5b',
+    textDecorationLine: 'underline',
+  },
+});
+
+export default styles;
--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import styles from './styles';
 import places from '../../../assets/data/feed';
 import CustomMarker from '../../components/CustomMarker';
+import PostCarouselItem from '../../components/PostCarouselItem';
+import {FlatList} from 'react-native-gesture-handler';
 
 const SearchResultsMaps = props => {
+  const [selectedPlaceId, setSelectPlaceId] = useState(null);
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
@@ -18,9 +21,23 @@ const SearchResultsMaps = props => {
           longitudeDelta: 0.8,
         }}>
         {places.map(place => (
-          <CustomMarker coordinate={place.coordinate} price={place.newPrice} />
+          <CustomMarker
+            isSelected={place.id === selectedPlaceId}
+            onPress={() => setSelectPlaceId(place.id)}
+            coordinate={place.coordinate}
+            price={place.newPrice}
+          />
         ))}
       </MapView>
+
+      <View style={{position: 'absolute', bottom: 40}}>
+        <FlatList
+          data={places}
+          renderItem={({item}) => <PostCarouselItem post={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
     </View>
   );
 };
